efbf45acefc5ffb129b451740ee4002b3f8673d7
Sol Pichirili <sol.pichirili@gmail.com>
2021-09-09 17:52:28 -0300

    desafio 1

---
+++ index.js
@@ -0,0 +1,44 @@
+class Usuario {
+    constructor(nombre, apellido, libros, mascotas) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = libros;
+        this.mascotas = mascotas;
+    }
+
+    getFullName() {
+        console.log(`${this.nombre} ${this.apellido}`)
+    }
+
+    addMascota(mascota) {
+        this.mascotas.push(mascota)
+        console.log(this.mascotas)
+    }
+
+    countMascotas() {
+        console.log(this.mascotas.length)
+    }
+
+    addBook(nombre, autor) {
+        this.libros.push({ nombre: nombre, autor: autor })
+        console.log(this.libros)
+    }
+
+    getBook() {
+        const nombresLibros = [];
+
+        this.libros.forEach(libro => {
+            nombresLibros.push(libro.nombre)
+        });
+
+        console.log(nombresLibros);
+    }
+}
+
+let usuario = new Usuario('Pedro', 'Martinez', [{ nombre: 'Rayuela', autor: 'Julio Cortázar' }, { nombre: 'Mi planta de naranja lima', autor: 'Juan José Vasconcelos' }], ['Penny Lane', 'Diego'])
+
+usuario.getFullName();
+usuario.countMascotas();
+usuario.addMascota('Leti');
+usuario.addBook('Crónicas Marcianas', 'Ray Bradbury');
+usuario.getBook();
